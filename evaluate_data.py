b273268d169e1f0c486a8227e4c2d48d7057f02d
TimZeliff <timzeliff@gmail.com>
2026-05-28 10:30:47 +1000

    update evaluate_name to include probability and count

--- evaluate_data.py
+++ evaluate_data.py
@@ -3,7 +3,13 @@ import requests
 names = {}
 
 def evaluate_name(name):
-  if (name not in names):
-    names[name] = requests.get(f"https://api.genderize.io?name={name}").json()["gender"]
-  
-  return names[name]
+    if (name not in names):
+        response = requests.get(f"https://api.genderize.io?name={name}").json()
+        
+        names[name] = {
+            "gender": response["gender"],
+            "probability": response["probability"],
+            "count": response["count"]
+        }
+
+    return names[name]
